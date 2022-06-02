fb72b9351e7106abecb80ef717010a0549d5ec75
AndreiValenzuela <87692245+AndreiValenzuela@users.noreply.github.com>
2022-06-03 01:18:34 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ author 'AC by: AxyDevelopmentsAC'
 description 'AxyDevelopmentsAC'
 
 versioncheck 'https://raw.githubusercontent.com/thelindat/tokyocity_inventory/main/fxmanifest.lua'
-version '2.0'
+version '3.0'
 
 
 client_scripts {
