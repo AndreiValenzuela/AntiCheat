890da07590489697469b27604e6c02902bf7768a
AndreiValenzuela <87692245+AndreiValenzuela@users.noreply.github.com>
2022-06-03 00:45:03 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'AC by: AxyDevelopmentsAC'
+description 'AxyDevelopmentsAC'
+
+versioncheck 'https://raw.githubusercontent.com/thelindat/tokyocity_inventory/main/fxmanifest.lua'
+version '2.0'
+
+
+client_scripts {
+	'config/*.lua',
+	'client/*.lua',
+}
+
+server_scripts {
+	'config/*.lua',
+	'server/*.lua',
+} 
+
+
+
+
+
+
+
+
+
